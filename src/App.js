242c91db4f0e19f58f75cbe5c93969183f08d66e
Enid Nyatichi <enidnyatichi1212@gmail.com>
2023-01-05 10:46:43 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import UseState from "./components/UseState"
 import UseEffect from "./components/UseEffect";
 import Counter from "./components/Counter";
+import FetchingData from "./components/FetchingData";
 
 function App() {
   return (
     <div className="App">
       {/* <UseState/> */}
       {/* <UseEffect/> */}
-      <Counter/>
+      {/* <Counter/> */}
+      <FetchingData/>
     </div>
   );
 }
--- src/components/FetchingData.js
+++ src/components/FetchingData.js
@@ -1,10 +1,34 @@
 import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 
 function FetchingData() {
-const [posts, setPosts] = useEffect("")
+// const [posts, setPosts] = useState([])
+const [post, setPost] = useState({})
+const [id, setId] = useState(1)
+useEffect(()=>{
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(res=>{
+        setPost(res.data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+//   axios.get("https://jsonplaceholder.typicode.com/posts")  
+//   .then(res=>{
+//     // console.log(res)
+//     setPosts(res.data)
+//   })
+//   .catch(err =>{
+//     console.log(err);
+//   })
+},[id])
   return (
     <div>
-
+    <input type="text" value={id} onChange={e => setId(e.target.value)} />
+<ul>
+    {/* {posts.map(post=><li key={post.id}>{post.title}</li>)} */}
+    {post.title}
+</ul>
     </div>
   )
 }
