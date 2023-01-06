71a34837c2f43b2731590970f38ed99cd2d32265
Enid Nyatichi <enidnyatichi1212@gmail.com>
2023-01-06 12:30:58 +0300

    Update

---
+++ src/components/context/UseContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// This is all about passing properties from the parent component down to the child components
+// Context provides a way of passing data through the component tree without having to pass the props down manually.
+
+function UseContext() {
+  return (
+    <div>UseContext</div>
+  )
+}
+
+export default UseContext
